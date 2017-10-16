00979ee003dad28783a95ba46afe0bcc3f3f1a5b
aceymomo <kobezhang333@vip.qq.com>
2017-10-16 19:41:07 +0800

    2017/10/16

---
+++ src/components/continaer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app2">
+      Count is {{ $store.state.count }}
+      <button @click="increment">+5</button>
+      <button @click="decrement">-3</button>
+  </div>
+</template>
+
+<script>
+import {mapGetters,mapActions} from 'vuex'
+export default{
+    computed:mapGetters([
+
+    ]),
+    methods:mapActions([
+        'increment',
+        'decrement'
+    ])
+}
+</script>
